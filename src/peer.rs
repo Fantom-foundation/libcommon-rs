4dd6aff9d2cd8df457a15a753eb0c096e8e3c3f2
Maxim Zakharov <5158255+Maxime2@users.noreply.github.com>
2019-08-02 15:46:21 +1000

    change Peer from struct to trait

--- src/peer.rs
+++ src/peer.rs
@@ -4,17 +4,22 @@ use std::fmt::Debug;
 pub trait PeerId: Eq + Ord + Clone + Debug + Send + Serialize + Sync {}
 impl<N> PeerId for N where N: Eq + Ord + Clone + Debug + Send + Serialize + Sync {}
 
-#[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct Peer<Id: PeerId> {
-    // unique id of the peer
-    #[serde(rename = "PubKeyHex")]
-    pub id: Id,
-    // network / transport address of the peer, in URI format, https://tools.ietf.org/html/rfc3986
-    #[serde(rename = "NetAddr")]
-    pub net_addr: String,
+//#[derive(Clone, Debug, Serialize, Deserialize)]
+//pub struct Peer<Id: PeerId> {
+//    // unique id of the peer
+//    #[serde(rename = "PubKeyHex")]
+//    pub id: Id,
+//    // network / transport address of the peer, in URI format, https://tools.ietf.org/html/rfc3986
+//    #[serde(rename = "NetAddr")]
+//    pub net_addr: String,
+//}
+
+pub trait Peer<Id: PeerId> {
+    fn get_id(&self) -> Id;
+    fn get_net_addr(&self) -> String;
 }
 
 pub trait PeerList<Id: PeerId, Error>: Iterator {
-    fn add(&mut self, p: Peer<Id>) -> std::result::Result<(), Error>;
+    fn add(&mut self, p: dyn Peer<Id>) -> std::result::Result<(), Error>;
     fn get_peers_from_file(&mut self, json_peer_path: String) -> std::result::Result<(), Error>;
 }
